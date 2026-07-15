da1d19a0134fa96e98c7b8ff068c4c2bad7921e7
omidbaharifar <weblax.ir@gmail.com>
2026-07-15 11:03:15 +0330

    feat: create basic base

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "تسک لایو کدینگ شرکت همیار حامی",
+  title: "تسک لایو کدینگ",
 };
 
 export default function RootLayout({
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,3 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
     <div className="text-xl font-bold flex items-center justify-center h-dvh">
